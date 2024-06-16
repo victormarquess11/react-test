7c2302bf9a2976a8aa6b30109f400bed92cc5017
victormarquess11 <victormarquess11@gmail.com>
2024-06-16 15:30:32 -0300

    [ADD] mantine ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 import "./index.css";
+import { theme } from "./theme";
 
 import Square from "./components/Board.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Square></Square>
+        <MantineProvider theme={theme}>
+            <Square></Square>
+        </MantineProvider>
     </React.StrictMode>,
 );
