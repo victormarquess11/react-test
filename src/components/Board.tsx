76d494549a03b7b281b71cf71bcb6bd34a051422
victormarquess11 <victormarquess11@gmail.com>
2024-06-16 14:46:25 -0300

    [ADD] funcionalidade basica e botao restart

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,27 +1,79 @@
-import "./Board.css";
+import { useState } from "react";
 
-function Square({ value }) {
-    return <button className="square">{value}</button>;
-}
+import "./Board.css";
+import Square from "./Square.tsx";
+
+export type SquareSymbols = "X" | "O" | null;
 
 export default function Board() {
+    const [xIsNext, setXIsNext] = useState<boolean>(true);
+    const [squares, setSquares] = useState<SquareSymbols[]>(
+        Array(9).fill(null),
+    );
+
+    function handleClick(i: number) {
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+    }
+
+    function handleRestart() {
+        setSquares(Array(9).fill(null));
+    }
+
     return (
-        <div className="ml-2 mt-2 flex h-screen w-screen flex-col items-center justify-center">
-            <div className="board-row">
-                <Square value="1" />
-                <Square value="2" />
-                <Square value="3" />
+        <div className="flex h-screen w-screen flex-col items-center justify-center">
+            <h1 className="mb-3 text-4xl">Tic Tac Toe</h1>
+            <div className="board-row-container">
+                <div className="board-row">
+                    <Square
+                        value={squares[0]}
+                        onSquareClick={() => handleClick(0)}
+                    />
+                    <Square
+                        value={squares[1]}
+                        onSquareClick={() => handleClick(1)}
+                    />
+                    <Square
+                        value={squares[2]}
+                        onSquareClick={() => handleClick(2)}
+                    />
+                </div>
+                <div className="board-row">
+                    <Square
+                        value={squares[3]}
+                        onSquareClick={() => handleClick(3)}
+                    />
+                    <Square
+                        value={squares[4]}
+                        onSquareClick={() => handleClick(4)}
+                    />
+                    <Square
+                        value={squares[5]}
+                        onSquareClick={() => handleClick(5)}
+                    />
+                </div>
+                <div className="board-row"></div>
+
+                <Square
+                    value={squares[6]}
+                    onSquareClick={() => handleClick(6)}
+                />
+                <Square
+                    value={squares[7]}
+                    onSquareClick={() => handleClick(7)}
+                />
+                <Square
+                    value={squares[8]}
+                    onSquareClick={() => handleClick(8)}
+                />
             </div>
-            <div className="board-row">
-                <Square value="4" />
-                <Square value="5" />
-                <Square value="6" />
-            </div>
-            <div className="board-row">
-                <Square value="7" />
-                <Square value="8" />
-                <Square value="9" />
-            </div>
+            <button onClick={handleRestart}>Restart</button>
         </div>
     );
 }
---
+++ src/components/Square.tsx
@@ -0,0 +1,14 @@
+import type { SquareSymbols } from "./Board.tsx";
+
+type SquareProps = {
+    value: SquareSymbols;
+    onSquareClick: () => void;
+};
+
+export default function Square({ value, onSquareClick }: SquareProps) {
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
+}
